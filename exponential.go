a5079e3ceab3e0dfe56b37040dd27601c447483d
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2022-04-11 03:34:44 +0000

    Initial code

---
+++ exponential.go
@@ -0,0 +1,23 @@
+package exponential
+
+import (
+	"time"
+
+	"github.com/MatthewDolan/jitter"
+)
+
+// Delay returns a function that iterative returns the next delay in an exponential series.
+func Delay(initialDelay time.Duration, maxDelay time.Duration, factor float64) func() time.Duration {
+	delay := initialDelay
+
+	return func() time.Duration {
+		jitteredDelay := jitter.UpTo(delay)
+
+		delay = time.Duration(float64(initialDelay) * factor)
+		if delay > maxDelay {
+			delay = maxDelay
+		}
+
+		return jitteredDelay
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MatthewDolan/go-template
 
 go 1.18
+
+require github.com/MatthewDolan/jitter v0.1.0
